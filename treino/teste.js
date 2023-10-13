588762009ba24f5598062a59760c9d82ad5a2910
sp4cecoder <145162127+sp4cecoder@users.noreply.github.com>
2023-10-13 15:54:48 -0300

    fiz a aula 13

---
+++ treino/teste.js
@@ -0,0 +1,13 @@
+var j = 1545
+if (j == 1545) {
+    var num = 1
+    while (num <= 10) {
+        console.log(`Contagem: ${num}`)
+        num++
+        if (num == 10) {
+            console.log('Parabéns')
+        }
+    }
+} else {
+    console.log('Não foi dessa vez')
+}
